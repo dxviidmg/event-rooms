cd97e48d335c9f802a1b1759cc00ef5a4c79c5ff
David <davidmejia.05@hotmail.com>
2022-10-06 19:30:02 -0500

    x

--- event_rooms/settings.py
+++ event_rooms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)&_2h0wht-d**$h+dtm5aml@a62)lvu^!_2kdjzpm#!spb*=!u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokuapp.com']
+ALLOWED_HOSTS = ['event-rooms.herokuapp.com']
 
 
 # Application definition
